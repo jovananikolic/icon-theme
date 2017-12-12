e38f52972efdecfbb2a8d90c2b4296f4d51ea980
Jovana Nikolic <jovana.nikolic97@yahoo.com>
2017-12-12 14:50:19 +0100

    Sredjivanje countera

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    
-    $('.navbar-nav a').click(function () {
+$(document).ready(function () {
+
+    $('.navbar-nav a, .landing .container > a').click(function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 1250);
     });
-    
+
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 150) {
@@ -18,4 +18,38 @@ $(document).ready(function(){
             $('header').css('background-color', 'rgba(0,0,0,0.3)');
         }
     });
+
+
+//ROUND SLIDER FOR PERCENT
+    $(window).scroll(function () {
+        percent();
+    });
+    percent();
+
+    function percent() {
+        var vh = $(window).height();
+        var scroll = $(window).scrollTop();
+
+        $('.cont').each(function () {
+            var position = $(this).offset().top;
+
+            if (position < vh + scroll - 100) {
+                var val = parseInt($(this).attr('data-pct'));
+                var $circle = $(this).find('.svg .bar');
+
+
+                var r = $circle.attr('r');
+                var c = Math.PI * (r * 2);
+                var pct = ((100 - val) / 100) * c;
+
+                $circle.css({strokeDashoffset: pct});
+            }
+
+
+
+//            $('.cont').attr('data-pct', val);
+
+        });
+    }
+
 });
